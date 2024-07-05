8135b1ba111823096697bd4e879087ceb0a1c2c6
mahanthnaidu <95294360+mahanthnaidu@users.noreply.github.com>
2024-07-05 11:10:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,48 @@
+import warnings
 import pandas as pd
+from bn_rca.bn_algo import bn_rca_func  
+from ht_rca.ht_algo import ht_rca_func
+from pc_graph.pc_algo import pc_causal_graph
 from ges_graph.ges_algo import ges_causal_graph
-from rcd.rcd import root_causal_discovery
+from random_walk_rca.random_walk_algo import random_walk_func
+from background_graph.causal_graph import domain_knowledge_graph
 from epsilon_diagnosis.epsilon_diagnosis import epsilon_diagnosis_function
 
-def add_anomaly_column(input_file, dev=0.3):
+warnings.filterwarnings('ignore')
+
+def add_anomaly_column(input_file, dev=0.07):
     df = pd.read_csv(input_file)
     df['anomaly'] = 0
-    normal_df = df.iloc[:7].copy()
+    normal_df = df.iloc[:24].copy()
     abnormal_df = pd.DataFrame(columns=df.columns)
-    for i in range(7, len(df)):
+    abs_dev_percentages = []
+    for i in range(24, len(df)):
         current_row = df.iloc[i:i+1]
-        last_7_avg = normal_df.iloc[-7:, 1].mean()   
-        if abs(current_row.iloc[0, 1] - last_7_avg) / last_7_avg >= dev:
+        last_24_avg = normal_df.iloc[-24:, 1].mean()
+        abs_dev_percent = (abs(current_row.iloc[0, 1] - last_24_avg) / last_24_avg) * 100
+        abs_dev_percentages.append(abs_dev_percent)
+
+        if abs_dev_percent >= dev * 100:
             df.at[i, 'anomaly'] = 1
             abnormal_df = pd.concat([abnormal_df, current_row], ignore_index=True)
         else:
             normal_df = pd.concat([normal_df, current_row], ignore_index=True)
-    normal_df.to_csv('normal_dataset.csv', index=False)
-    abnormal_df.to_csv('abnormal_dataset.csv', index=False)
+       
+    normal_df.to_csv('datasets/normal_dataset.csv', index=False)
+    abnormal_df.to_csv('datasets/abnormal_dataset.csv', index=False)
     df.to_csv(input_file[:-4] + '_with_anomaly_label.csv', index=False)
-    return df,normal_df,abnormal_df
+    
+    return df, normal_df, abnormal_df, abs_dev_percentages
 
 class RCAFramework:
-    def __init__(self,dataset,k):
+    def __init__(self, dataset, k, background_knowledge):
         self.model_type = None
         self.algorithm_type1 = None
         self.algorithm_type2 = None
         self.dataset = dataset
         self.num_root_causes = k
         self.epsilon_diagnosis_type = None
+        self.background_knowledge = background_knowledge
     
     def select_model(self):
         print("Select RCA Model:")
@@ -36,10 +50,10 @@ class RCAFramework:
         print("2. Two-Phase Model")
         choice = int(input("Enter your choice (1 or 2): "))
         if choice == 1:
-            self.model_type = 'one-phase'
+            self.model_type = 'One-Phase'
             self.select_one_phase_algorithm()
         elif choice == 2:
-            self.model_type = 'two-phase'
+            self.model_type = 'Two-Phase'
             self.select_two_phase_algorithm()
         else:
             print("Invalid choice. Please select 1 or 2.")
@@ -51,91 +65,116 @@ class RCAFramework:
         print("2. Epsilon Diagnosis")
         choice = int(input("Enter your choice (1 or 2): "))
         if choice == 1:
-            self.algorithm_type1 = 'rcd'
+            self.algorithm_type1 = 'RCD'
         elif choice == 2:
-            self.algorithm_type1 = 'epsilon'
+            self.algorithm_type1 = 'Epsilon Diagnosis'
         else:
             print("Invalid choice. Please select 1 or 2.")
             self.select_one_phase_algorithm()
     
     def select_two_phase_algorithm(self):
         print("Select Algorithm for Graph Generation in Two-Phase Model:")
-        print("1. GES (Greedy Equivalence Search)")
-        print("2. PC (Peter and Clark Algorithm)")
-        choice1 = int(input("Enter your choice (1 or 2): "))
+        print("1. Domain Knowledge Graph")
+        print("2. GES (Greedy Equivalence Search)")
+        print("3. PC (Peter and Clark Algorithm)")
+        choice1 = int(input("Enter your choice (1, 2, or 3): "))
         if choice1 == 1:
-            self.algorithm_type1 = 'ges'
+            self.algorithm_type1 = 'Domain Knowledge Graph'
         elif choice1 == 2:
+            self.algorithm_type1 = 'GES'
+        elif choice1 == 3:
             self.algorithm_type1 = 'PC'
         else:
-            print("Invalid choice. Please select 1 or 2.")
+            print("Invalid choice. Please select 1, 2, or 3.")
             self.select_two_phase_algorithm()
-        
+
         print("Select Algorithm for Finding Root Causes in Two-Phase Model:")
         print("1. Random Walk")
         print("2. Hypothesis Testing")
-        choice2 = int(input("Enter your choice (1 or 2): "))
+        print("3. Bayesian Network")
+        choice2 = int(input("Enter your choice (1, 2, or 3): "))
         if choice2 == 1:
-            self.algorithm_type2 = 'X'
+            self.algorithm_type2 = 'Random Walk'
         elif choice2 == 2:
-            self.algorithm_type2 = 'Y'
+            self.algorithm_type2 = 'Hypothesis Testing'
+        elif choice2 == 3:
+            self.algorithm_type2 = 'Bayesian Network'
         else:
-            print("Invalid choice. Please select 1 or 2.")
+            print("Invalid choice. Please select 1, 2, or 3.")
             self.select_two_phase_algorithm()
     
     def run_rca(self):
         print("Running RCA with the following parameters:")
         print(f"Model Type: {self.model_type}")
-        if self.model_type == 'one-phase':
+        if self.model_type == 'One-Phase':
             print(f"Algorithm Type: {self.algorithm_type1}")
-            if self.algorithm_type1 == 'rcd':
+            if self.algorithm_type1 == 'RCD':
                 print("Implementing RCD algorithm for one-phase RCA.")
-                df,normal_df,abnormal_df = add_anomaly_column(self.dataset)
-                root_causal_discovery(normal_df_date=normal_df,anomalous_df_date=abnormal_df,k=self.num_root_causes)
+                df, normal_df, abnormal_df, abs_dev_percentages = add_anomaly_column(self.dataset)
+                # root_causal_discovery(normal_df=normal_df, abnormal_df=abnormal_df, k=self.num_root_causes)
                 
-            elif self.algorithm_type1 == 'epsilon':
+            elif self.algorithm_type1 == 'Epsilon Diagnosis':
                 print("Implementing Epsilon Diagnosis algorithm for one-phase RCA.")
                 print("Select Statistical Test Type:")
                 print("1. ks")
                 print("2. levene")
-                print("3. kruskal")
-                choice = int(input("Enter your choice (1 or 2 or 3): "))
+                print("3. ttest_ind_from_stats")
+                print("4. ks_2samp")
+                print("5. ranksums")
+                print("6. kruskal")
+                choice = int(input("Enter your choice (1, 2, 3, 4, 5, or 6): "))
                 if choice == 1:
                     self.epsilon_diagnosis_type = 'ks'
                 elif choice == 2:
                     self.epsilon_diagnosis_type = 'levene'
                 elif choice == 3:
+                    self.epsilon_diagnosis_type = 'ttest_ind_from_stats'
+                elif choice == 4:
+                    self.epsilon_diagnosis_type = 'ks_2samp'
+                elif choice == 5:
+                    self.epsilon_diagnosis_type = 'ranksums'
+                elif choice == 6:
                     self.epsilon_diagnosis_type = 'kruskal'
                 else:
-                    print("Invalid choice. Please select 1 or 2 or 3.")
-                df,normal_df,abnormal_df = add_anomaly_column(self.dataset)
-                epsilon_diagnosis_function(df=df,test=self.epsilon_diagnosis_type,k=self.num_root_causes)
+                    print("Invalid choice. Please select 1, 2, 3, 4, 5, or 6.")
+                df, normal_df, abnormal_df, abs_dev_percentages = add_anomaly_column(self.dataset)
+                epsilon_diagnosis_function(df=df, test=self.epsilon_diagnosis_type, k=self.num_root_causes, dev=abs_dev_percentages)
 
-        elif self.model_type == 'two-phase':
+        elif self.model_type == 'Two-Phase':
             print(f"Algorithm Type for Graph Generation: {self.algorithm_type1}")
             print(f"Algorithm Type for Finding Root Causes: {self.algorithm_type2}")
             graph_df = None
-            if self.algorithm_type1 == 'ges':
+            df, normal_df, abnormal_df, abs_dev_percentages = add_anomaly_column(self.dataset)
+            if self.algorithm_type1 == 'Domain Knowledge Graph':
+                print("Generating causal graph using domain knowledge graph.")
+                graph_df = domain_knowledge_graph(background_file=self.background_knowledge, variable_names=df.columns[1:])
+
+            elif self.algorithm_type1 == 'GES':
                 print("Generating causal graph using GES algorithm.")
-                df,normal_df,abnormal_df = add_anomaly_column(self.dataset)
-                graph_df = ges_causal_graph(normal_df=normal_df)
-                # print(graph_df)
-                # print("Heheheheheh")
+                graph_df = ges_causal_graph(normal_df=normal_df, background_file=self.background_knowledge)
+
             elif self.algorithm_type1 == 'PC':
                 print("Generating causal graph using PC algorithm.")
-                # Implement PC logic
+                graph_df = pc_causal_graph(normal_df=normal_df, background_file=self.background_knowledge)
             
-            # Implement root cause finding logic based on self.algorithm_type2
-        
+            if self.algorithm_type2 == 'Random Walk':
+                print("Implementing random walk algorithm for two-phase RCA.")
+                random_walk_func(graph_df=graph_df, normal_df=normal_df, abnormal_df=abnormal_df, root_cause_top_k=self.num_root_causes)
+
+            elif self.algorithm_type2 == 'Hypothesis Testing':
+                print("Implementing hypothesis testing algorithm for two-phase RCA.")
+                ht_rca_func(graph_df=graph_df, normal_df=normal_df, abnormal_df=abnormal_df, root_cause_top_k=self.num_root_causes)
+
+            elif self.algorithm_type2 == 'Bayesian Network':
+                print("Implementing Bayesian Network algorithm for two-phase RCA.")
+                bn_rca_func(graph_df=graph_df, normal_df=normal_df, abnormal_df=abnormal_df, root_cause_top_k=self.num_root_causes)
+
         print("RCA process completed.")
     
-    def epsilon_diagnosis(self, dataset_file, test_type, k):
-        df = pd.read_csv(dataset_file)
-        epsilon_diagnosis_function(df, test_type, k)
-    
 if __name__ == "__main__":
-    dataset = 'main_dataset.csv'
-    k = 5
-    rca = RCAFramework(dataset,k)
+    dataset = 'datasets/final_24.csv'
+    background_knowledge = 'datasets/background_knowledge.csv'
+    k = 10
+    rca = RCAFramework(dataset, k, background_knowledge)
     rca.select_model()
     rca.run_rca()
